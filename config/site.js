dff769713229390a17df44832b0f2b0b63be21d5
minouedesu <m.inoue@yoake-tokyo.jp>
2024-10-24 14:49:09 +0900

    test

--- config/site.js
+++ config/site.js
@@ -17,21 +17,21 @@ module.exports = {
     'native',
     'serverless',
   ],
-  title: "Maxime Heckel's Blog",
-  titleAlt: 'Maxime Heckel | Blog',
+  title: "Masahiro's Blog",
+  titleAlt: 'Masahiro | Blog',
   description:
-    "Hi I'm Maxime, and this is my blog. Here, I share through my writing my experience as a frontend engineer and everything I'm learning about on React, Shaders, React Three Fiber, Framer Motion, and more.",
-  url: 'https://blog.maximeheckel.com', // Site domain without trailing slash
-  siteUrl: 'https://blog.maximeheckel.com/', // url + pathPrefix
+    "Hi I'm Masahiro, and this is my blog. Here, I share through my writing my experience as a frontend engineer and everything I'm learning about on React, Shaders, React Three Fiber, Framer Motion, and more.",
+  url: 'https://masahiroinoue.jp', // Site domain without trailing slash
+  siteUrl: 'https://masahiroinoue.jp/', // url + pathPrefix
   siteLanguage: 'en', // Language Tag on <html> element
   logo: 'src/static/logo/logo.png',
   image: 'https://blog.maximeheckel.com/static/og/main-og-image.png',
   favicon: 'static/favicon.png', // Manifest favicon generation
   shortName: 'MaximeHeckel', // Shortname for manifest, must be shorter than 12 characters
-  author: 'Maxime Heckel', // Author for schemaORGJSONLD
+  author: 'Masahiro', // Author for schemaORGJSONLD
   themeColor: '#000000',
   backgroundColor: '#ffffff',
-  twitter: '@MaximeHeckel', // Twitter Username
-  twitterDesc:
-    'Maxime Heckel is a lead frontend engineer and space enthusiast currently based in New York.',
+  // twitter: '@MaximeHeckel', // Twitter Username
+  // twitterDesc:
+  //   'Maxime Heckel is a lead frontend engineer and space enthusiast currently based in New York.',
 };
